ce024417e238d4ce8a027e7bd7a6816450aa2e4f
msgumtow <71787320+msgumtow@users.noreply.github.com>
2021-03-09 11:38:10 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,15 @@
+/**
+ * PSEUDOCODE:
+ * 
+ * On start, the dollars will start at $5 and the cents will start at $0.00.
+ * 
+ * When button A is pressed, the amount of cents will change by 1 to increase.
+ * 
+ * When button B is pressed, the amount of cents will change by -1 to decrease. 
+ * 
+ * Forever, the microbit will show the total amount of dollars + cents.
+ */
+let dollars = 5
+basic.forever(function () {
+	
+})
